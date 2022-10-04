fb49e82bc4d6fcf1d2ab9d548f640ce61162218d
kiptook <Kiptook2020@gmail.com>
2022-10-04 07:52:14 +0300

    initial commit

---
+++ app/src/main/java/emtech/ui/jetpactcompose/MainActivity.kt
@@ -0,0 +1,35 @@
+package emtech.ui.jetpactcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import emtech.ui.jetpactcompose.ui.theme.JetpactComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetpactComposeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MessageCard("Welcome to Jetpact Compose material3")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MessageCard(name: String) {
+    Text(text = "Hello $name!")
+}
